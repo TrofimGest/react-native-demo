8d686410a87ae284e0180c8e13da3778c41cf4f8
TrofimGest <gubaoslika@gmail.com>
2022-06-09 00:42:02 +0300

    feat: add navbar component

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native';
-import { useState } from 'react';
+import { Navbar } from './src/Navbar';
 
 export default function App() {
-  const [color, setColor] = useState('blue');
-
-  function handlePress() {
-    if (color === 'blue') {
-      setColor('red');
-    } else if (color === 'red') {
-      setColor('black');
-    } else if (color === 'black') {
-      setColor('green');
-    } else if (color === 'green') {
-      setColor('blue');
-    }
-  }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <StatusBar style='auto' />
-      <Button onPress={handlePress} title={`${color} button`} color={color} />
+      <Navbar title='ToDo App' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'green',
-    fontSize: 30,
-  },
+  container: {},
 });
